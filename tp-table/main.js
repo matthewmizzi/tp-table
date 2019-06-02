bd9dcb3b677dd82d5fb188f8da7001583125ccac
DESKTOP-K97G691\Matthew <matthewmizzi1989@gmail.com>
2019-06-02 21:11:36 +0200

    adding and tested functions for displaying the borders and rows

--- tp-table/main.js
+++ tp-table/main.js
@@ -1,5 +1,22 @@
 let test1 = [43,35,80,123,12345,44,8,5,24,3,22,35];
-let test2 = [43];
+let test2 = [4];
+
+let printBorder = (array, max) => {
+
+    //just print the border.  If the index is 0 it means that an extra + needs to be added at the start
+    return array.map((number,index) => index == 0 ? `+${'-'.repeat(max)}+` : `${'-'.repeat(max)}+`)
+}
+
+let printRow = (array, max) => {
+
+    //just print the row.  If the index is 0 it means that an extra | needs to be added at the start
+    return array.map((number, index) => {
+        
+        //calculate amount of spaces needed for the current tab
+        const difference = max - number.toString().length;
+        return index == 0 ? `|${' '.repeat(difference) + number}|` : `${' '.repeat(difference) + number}|`;
+    })
+}
 
 let setupRows = (array, rowNumber) => {
 
@@ -26,6 +43,12 @@ let solution = (array, rowNumber=1) => {
 
     //split the array into multiple array per row
     const splitArray = setupRows(array, rowNumber);
+
+    let table = "";
+
+    table += `${printBorder(array, max).join("")}\n`;
+    table += `${printRow(array, max).join("")}\n`;
+    console.log(table);
 
     debugger;
 }
